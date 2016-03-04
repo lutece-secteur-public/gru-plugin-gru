cc91d3f0d69188600f70348fbbbefc83fea6f4ac
ryahiaoui <ryahiaoui@users.noreply.github.com>
2016-03-04 17:11:38 +0000

    init script for sqlpage
    git-svn-id: https://dev.lutece.paris.fr/svn/lutece/applications/gru/plugin-gru/trunk@81328 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/crm/plugin/init_crm_demand_type.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO `crm_demand_type` VALUES (101,'Sollicitaion','Sollicitaion','','',2,0,NULL,NULL,'all','none',0,'',1,1,1),(102,'Réclamation','Réclamation','','',1,0,NULL,NULL,'all','none',0,'',1,1,1);
