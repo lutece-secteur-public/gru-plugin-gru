8a21332b9fbf617f69aa232664e8694c63fbd197
MOIRE <cyril.moire@atos.net>
2016-04-22 10:55:09 +0200

    GRU-9 : create a post on the fly because IE does not correctly manage location.replace

--- webapp/js/plugins/gru/gru.js
+++ webapp/js/plugins/gru/gru.js
@@ -22,3 +22,14 @@ $(function(){
 
 	});
 });
+
+function redirectOnClick( element ){
+	if ( $(element).attr("data-url") != undefined ){
+	    var form = $('<form>', 
+	    	{ 'action':$(element).attr("data-url"),
+	    	'method':'post'}
+       );
+       form.appendTo('body');
+       form.submit().remove();
+    }
+}
