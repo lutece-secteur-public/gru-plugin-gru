eb37a8000c6e7190cf3534c2dd605df95efef921
MOIRE <cyril.moire@atos.net>
2016-05-19 11:05:13 +0200

    GRU-11 : correct sample if foreign keys are used

--- src/sql/plugins/html/core/init_core_html_data_sample_gru.sql
+++ src/sql/plugins/html/core/init_core_html_data_sample_gru.sql
@@ -1,12 +1,13 @@
 --
 -- Dumping data for table core_portlet
 --
-
+DELETE FROM `core_portlet` WHERE id_portlet IN (4,83,85,87,88,89,94,95);
 INSERT INTO `core_portlet` VALUES (85,'CHILDPAGES_PORTLET',5,'Pages filles','2007-11-24 16:15:10',0,1,5,300,0,'2007-11-24 16:14:58',1,NULL,15),(87,'CHILDPAGES_PORTLET',3,'Pages filles','2007-11-24 16:21:01',0,1,5,300,0,'2007-11-24 16:19:50',1,NULL,15),(88,'CHILDPAGES_PORTLET',10,'Pages filles','2007-11-24 16:20:37',0,1,5,301,0,'2007-11-24 16:20:37',1,NULL,15),(89,'CHILDPAGES_PORTLET',9,'Pages filles','2007-11-24 16:23:06',0,1,5,301,0,'2007-11-24 16:21:47',1,NULL,15),(83,'CHILDPAGES_PORTLET',1,'Pages filles','2007-11-24 15:11:33',0,1,5,300,0,'2007-11-24 15:11:33',1,NULL,15),(94,'FORM_PORTLET',11,'Questionnaire','2009-06-16 10:55:48',0,1,1,1500,1,'2009-06-16 10:55:48',1,NULL,15),(4,'HTML_PORTLET',2,'Page 1','2014-06-08 16:28:50',0,1,1,100,0,'2014-06-08 16:27:59',0,'none',273),(95,'HTML_PORTLET',1,'Liens','2016-02-16 18:34:30',0,1,1,100,0,'2016-02-16 17:11:11',0,'none',4369);
 
 --
 -- Dumping data for table html_portlet
 --
-
+DELETE FROM `html_portlet` WHERE id_portlet IN (4,95);
 INSERT INTO `html_portlet` VALUES (4,'<h2>Here is a child page sample.</h2>\n<p>&#160;</p>\n<p>This page is divided into 2 columns.</p>\n<p>In each column you can publish several and different portlets :\nHTML, documents, ...</p>\n<p>This page can also have childs.</p>\n<p>&#160;</p>\n<p>&#160;</p>\n'),(95,'<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html>\n<head>\n<meta name=\"generator\" content=\n\"HTML Tidy for Java (vers. 26 sept. 2004), see www.w3.org\">\n<title></title>\n</head>\n<body>\n<p>&nbsp;</p>\n<p>&nbsp;</p>\n<p>&nbsp;</p>\n<p>&nbsp;</p>\n<ul>\n<li><a href=\"jsp/site/Portal.jsp?page=ticket&returnUrl=Portal.jsp?page=crm\">Cr&eacute;ation d\'une\nsollicitation</a></li>\n<li><a title=\"Mes demandes\" href=\"jsp/site/Portal.jsp?page=crm\">Mes\ndemandes</a></li>\n</ul>\n</body>\n</html>\n');
+DELETE FROM `html_portlet` WHERE id_portlet IN (95);
 INSERT INTO `html_portlet` VALUES (95,'<div class=\"row\">&#160; \n<div class=\"col-md-12 col-lg-12\">&#160; \n<div class=\"layout-wrapper\" style=\"margin-top: 50px;\">\n<a class=\"btn btn-primary\"\nhref=\"jsp/site/Portal.jsp?page=ticket&amp;returnUrl=Portal.jsp?page=crm\">CrÃ©ation\nd\'une sollicitation</a> \n<a class=\"btn btn-primary\" title=\"Mes demandes\"\nhref=\"jsp/site/Portal.jsp?page=crm\">Mes\ndemandes</a></div></div></div>\n');
